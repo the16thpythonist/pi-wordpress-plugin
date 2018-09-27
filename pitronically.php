7778215edd6117d8285317567a31360188d15183
the16thpythonist <jonseb1998@gmail.com>
2018-09-27 17:50:34 +0200

    initial commit

---
+++ pitronically.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Pitronically
+ * Plugin URI: http://google.com
+ * Description: The Plugin containing the custom functionality for the Pitronically website
+ * Author: Jonas Teufel
+ * Version: 0.0.0.0
+ * Author URI: http://google.com
+ * License: GPLv2 or later
+ */
